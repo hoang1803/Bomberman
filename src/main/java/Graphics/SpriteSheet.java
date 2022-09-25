c63afade79bfdabe5ffc18d40557d387534686a9
hoang1803 <21020073@vnu.edu.vn>
2022-09-25 16:12:42 +0700

    Update SpriteSheet.java

---
+++ src/main/java/Graphics/SpriteSheet.java
@@ -0,0 +1,43 @@
+package Graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class SpriteSheet {
+    private String path;
+    public final int SIZE;
+    public int[] spritePixel;
+    public BufferedImage image;
+
+    /**
+     * Load image pixel into spritePixel.
+     */
+    private void load() {
+        try {
+            URL a = SpriteSheet.class.getResource(path);
+            image = ImageIO.read(a);
+            int width = image.getWidth();
+            int height = image.getHeight();
+            image.getRGB(0, 0, width, height, spritePixel, 0, width);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Constructor.
+     * @param path path
+     * @param SIZE SpriteSheet size
+     */
+    public SpriteSheet(String path, int SIZE) {
+        this.path = path;
+        this.SIZE = SIZE;
+        spritePixel = new int[SIZE * SIZE];
+        load();
+    }
+
+    public static SpriteSheet tiles = new SpriteSheet("/textures/classic.png", 256);
+}
