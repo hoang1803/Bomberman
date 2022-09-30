61ac32be5a6ff453e939d3db24682275099a8c55
hoang1803 <21020073@vnu.edu.vn>
2022-09-30 19:41:05 +0700

    Update RunBomberman.java

--- src/main/java/GameRunner/RunBomberman.java
+++ src/main/java/GameRunner/RunBomberman.java
@@ -1,6 +1,10 @@
 package GameRunner;
 
+import Entity.Block.Grass;
+import Entity.Block.Wall;
+import Entity.Entity;
 import Entity.Figure.Figure;
+import Graphics.Map;
 import Graphics.Sprite;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -14,12 +18,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RunBomberman extends Application {
-    public static final int HEIGHT = 15;
-    public static final int WIDTH = 25;
+    public static final int HEIGHT = 13;
+    public static final int WIDTH = 31;
 
     public static int level = 1;
     public static List<Figure> enemy = new ArrayList<>();
+    public static List<Entity> block = new ArrayList<>();
     public static Figure player;
+    public static char[][] objectMap;
 
     private GraphicsContext gc;
     private Canvas canvas;
@@ -30,6 +36,25 @@ public class RunBomberman extends Application {
     @Override
     public void start(Stage primaryStage) {
         canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+
+
         gc = canvas.getGraphicsContext2D();
+        Group group = new Group();
+        group.getChildren().add(canvas);
+
+        Scene scene = new Scene(group);
+
+        primaryStage.setTitle("Bomberman");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        render();
+    }
+
+    public void update() {
+
+    }
+
+    public void render() {
+
     }
 }
