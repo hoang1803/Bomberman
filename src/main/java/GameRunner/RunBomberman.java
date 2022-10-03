e140e232f3cc6af3867c13bea007845f3b0d3e3f
hoang1803 <21020073@vnu.edu.vn>
2022-10-03 19:13:32 +0700

    Update RunBomberman.java: Update move for character

--- src/main/java/GameRunner/RunBomberman.java
+++ src/main/java/GameRunner/RunBomberman.java
@@ -1,11 +1,15 @@
 package GameRunner;
 
+import Control.Move;
 import Entity.Block.Grass;
 import Entity.Block.Wall;
 import Entity.Entity;
+import Entity.Figure.Bomber;
 import Entity.Figure.Figure;
 import Graphics.Map;
 import Graphics.Sprite;
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -27,9 +31,17 @@ public class RunBomberman extends Application {
     public static Figure player;
     public static char[][] objectMap; // Include: WALL, BRICK, PORTAL, BOMB, GRASS
     public static int[][] killObject; // Bomb sau khi nổ thì lưu vết lửa vẫn còn ở đây.
+    public static Map map;
 
     private GraphicsContext gc;
     private Canvas canvas;
+
+    public static boolean isPause = false;
+
+    public static boolean running = true;
+
+    public static Stage stage = null;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -45,17 +57,54 @@ public class RunBomberman extends Application {
 
         Scene scene = new Scene(group);
 
+        map = new Map(level);
+        scene.setOnKeyPressed(keyEvent -> {
+            if (player.getLife() > 0) {
+                switch (keyEvent.getCode()) {
+                    case UP -> Move.moveUp(player);
+                    case DOWN -> Move.moveDown(player);
+                    case LEFT -> Move.moveLeft(player);
+                    case RIGHT -> Move.moveRight(player);
+                }
+            }
+        });
+
         primaryStage.setTitle("Bomberman");
         primaryStage.setScene(scene);
-        primaryStage.show();
-        render();
+        stage = primaryStage;
+        stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                if(running) {
+                    render();
+                    if(!isPause) {
+                        update();
+                    }
+                }
+            }
+        };
+        timer.start();
+        player = new Bomber(Figure.speed * 4, 2,"right", 3);
+
     }
 
     public void update() {
+        block.forEach(Entity::update);
+        player.update();
 
+        int delay = player.getDelayTime();
+        if (delay == 0) {
+           Move.figureRun(player);
+           delay = Figure.DELAY_TIME;
+        }
+        player.setDelayTime(delay - 1);
     }
 
     public void render() {
-
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        block.forEach(g -> g.render(gc));
+        player.render(gc);
     }
 }
