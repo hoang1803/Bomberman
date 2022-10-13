b726e2c413126ef8fdbb6f891da40f154466ff95
dquynh2811 <21020087@vnu.edu.vn>
2022-10-13 22:42:00 +0700

    Update Brick.java render grass

--- src/main/java/Entity/Block/Brick.java
+++ src/main/java/Entity/Block/Brick.java
@@ -1,7 +1,12 @@
 package Entity.Block;
 
 import Entity.Entity;
+import Graphics.Sprite;
 import javafx.scene.image.Image;
+
+import static GameRunner.RunBomberman.block;
+import static GameRunner.RunBomberman.killObject;
+import static Graphics.Sprite.SCALED_SIZE;
 
 public class Brick extends Entity {
     public Brick(int x, int y, Image image) {
@@ -10,6 +15,11 @@ public class Brick extends Entity {
 
     @Override
     public void update() {
-        //Write code here
+        for (Entity entity : block) {
+            if (entity instanceof Brick)
+                if (killObject[entity.getY() / SCALED_SIZE][entity.getX() / SCALED_SIZE] == 4) {    // At the element of the 2-dimensional killObject array with the value 4, Brick and Grass will appear
+                    entity.setImage(Sprite.grass.getFxImage());
+                }
+        }
     }
 }
