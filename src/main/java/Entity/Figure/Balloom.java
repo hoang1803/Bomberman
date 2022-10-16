3b21911e5be25d777f44444e79dc9ee7da72ce80
h335225 <21020016@vnu.edu.vn>
2022-10-17 00:29:18 +0700

    Insert auto transDirection

--- src/main/java/Entity/Figure/Balloom.java
+++ src/main/java/Entity/Figure/Balloom.java
@@ -1,71 +1,123 @@
 package Entity.Figure;
 
 import Control.Move;
-import Entity.Figure.Figure;
-import GameRunner.RunBomberman;
 import Graphics.Sprite;
 import javafx.scene.image.Image;
 
-import static GameRunner.RunBomberman.enemy;
-import static GameRunner.RunBomberman.player;
+import java.util.Random;
+
+import static GameRunner.RunBomberman.*;
+
 
 public class Balloom extends Figure {
     public int deadFrame = 1;
     private int countDead = 0;
-    private boolean isDead = false;
 
-    private boolean transDirection = false;
+    private boolean transDirect = false;
+    private Random rand = new Random();
+
+    private int timeLeft = 0;
 
     public Balloom() {
 
     }
 
-    //Update
     public Balloom(int currentSpeed, int currentFrame, int defaultDelayTime, String direction, int life) {
         super(currentSpeed, currentFrame, defaultDelayTime, direction, life);
-        super.setX(Sprite.SCALED_SIZE);
-        super.setY(Sprite.SCALED_SIZE);
         super.setImage(Sprite.balloom_left1.getFxImage());
         super.setDefaultCount(1);
-        super.setCurrentSpeed(5);
+        super.setCount(1);
     }
 
     public Balloom(int x, int y, Image img) {
         super(x, y, img);
     }
 
-    private void killBalloom(Figure figure) {
-        life = -1;
-        figure.setDelayTime(5);
-        figure.setImage(Sprite.balloom_dead.getFxImage());
-    }
-
-    /**
-     * Giao nhau.
-     */
-    private boolean hasIntersect(int pos1, int pos2) {
-        int esp = 5;
-        return (pos2 < pos1 + Sprite.SCALED_SIZE &&
-                pos1 + Sprite.SCALED_SIZE < pos2 + Sprite.SCALED_SIZE) ||
-                (pos1 < pos2 + Sprite.SCALED_SIZE &&
-                        pos2 + Sprite.SCALED_SIZE < pos1 + Sprite.SCALED_SIZE);
-    }
-
-    private void checkBomb() {
-        int size = Sprite.SCALED_SIZE;
-        int x = this.x / size;
-        int px = (this.x + size - 1) / size;
-        int y = this.y / size;
-        int py = (this.y + size - 1) / size;
-
-        int leftUp = RunBomberman.killObject[y][x];
-        int leftDown = RunBomberman.killObject[py][x];
-        int rightUp = RunBomberman.killObject[y][px];
-        int rightDown = RunBomberman.killObject[py][px];
-
-        if(leftUp != 0 || leftDown != 0 || rightUp != 0 || rightDown != 0) {
-            isDead = true;
+    @Override
+    public void setTransDirection(boolean transDirection) {
+        super.setTransDirection(transDirection);
+        if (transDirection) {
+            setDelayTime(0);
         }
+    }
+
+    public void transDirection() {
+        timeLeft--;
+        int pX = x / Sprite.SCALED_SIZE;
+        int pY = y / Sprite.SCALED_SIZE;
+
+        if (transDirect || pX * Sprite.SCALED_SIZE == x && pY * Sprite.SCALED_SIZE == y) {
+
+            if (transDirect || timeLeft <= 0) {
+                timeLeft = rand.nextInt(8) * 4;
+            } else {
+                return;
+            }
+            setTransDirection(false);
+
+            boolean left = ((pX >= 1) && (!Move.hasBlock(objectMap[pY][pX-1])));
+            boolean right = ((pX + 1 < WIDTH) && (!Move.hasBlock(objectMap[pY][pX+1])));
+            boolean up = ((pY >= 1) && (!Move.hasBlock(objectMap[pY-1][pX])));
+            boolean down = ((pY + 1 < HEIGHT) && (!Move.hasBlock(objectMap[pY+1][pX])));
+            int tmp = rand.nextInt(2) + 1;
+            switch (direction) {
+                case "left": {
+                    if (up && tmp == 1) {
+                        direction = "up";
+                    } else if (down && tmp == 2) {
+                        direction = "down";
+                    } else {
+                        direction = "right";
+                    }
+                }
+                break;
+
+                case "right": {
+                    if (up && tmp == 1) {
+                        direction = "up";
+                    } else if (down && tmp == 2) {
+                        direction = "down";
+                    } else {
+                        direction = "left";
+                    }
+                }
+                break;
+
+                case "up": {
+                    if (left && tmp == 1) {
+                        direction = "left";
+                    } else if (right && tmp == 2) {
+                        direction = "right";
+                    } else {
+                        direction = "down";
+                    }
+                }
+                break;
+
+                case "down": {
+                    if (left && tmp == 1) {
+                        direction = "left";
+                    } else if (right && tmp == 2) {
+                        direction = "right";
+                    } else {
+                        direction = "up";
+                    }
+                }
+                break;
+
+                default: {
+
+                }
+            }
+            System.out.println(direction);
+        }
+    }
+
+    @Override
+    protected void kill() {
+        life = -1;
+        this.setDelayTime(5);
+        this.setImage(Sprite.balloom_dead.getFxImage());
     }
 
     @Override
@@ -74,7 +126,7 @@ public class Balloom extends Figure {
 
         if (isDead) {
             life--;
-            killBalloom(this);
+            kill();
             isDead = false;
         }
 
