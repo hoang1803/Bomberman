7d71e871c407e8682b29fd755e4c52b8876b7fb8
hoang1803 <21020073@vnu.edu.vn>
2022-10-25 21:31:45 +0700

    Update Portal.java: Update method update()

--- src/main/java/Entity/Block/Portal.java
+++ src/main/java/Entity/Block/Portal.java
@@ -1,10 +1,18 @@
 package Entity.Block;
 
+import Entity.Figure.Bomber;
+import Entity.Figure.Figure;
+import GameRunner.RunBomberman;
+import Graphics.Map;
 import javafx.scene.image.Image;
 import Entity.Entity;
 
+import static GameRunner.RunBomberman.*;
+
+
 public class Portal extends Entity {
-    public static boolean IsPortal = false;
+    public static boolean isPortal = false;
+    private long waitingTime;
 
     /**
      * Constructor.
@@ -15,6 +23,27 @@ public class Portal extends Entity {
 
     @Override
     public void update() {
+        if (player.getX() == this.x && player.getY() == this.y && !isPortal) {
+            isPortal = true;
+            waitingTime = System.currentTimeMillis();
+        }
 
+        if (isPortal) {
+            long currentTime = System.currentTimeMillis();
+            // The loading page
+            System.out.println("Loading new map");
+            if (currentTime - waitingTime > 3000) {
+                player = new Bomber(Figure.speed * 4, 2, 5,"right", 1);
+                Bomb.countBomb = 1;
+                Bomb.range = 0;
+                switch (level) {
+                    case 1 -> Bomb.bombNumber = 20;
+                    case 2 -> Bomb.bombNumber = 30;
+                    case 3 -> Bomb.bombNumber = 40;
+                }
+                wait = false;
+                map = new Map(level);
+            }
+        }
     }
 }
