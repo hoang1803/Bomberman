6a753ead7ca99bb39e015a6072188f4814d10332
h335225 <21020016@vnu.edu.vn>
2022-10-26 22:08:25 +0700

    update nextLevel

--- src/main/java/Level/NextLevel.java
+++ src/main/java/Level/NextLevel.java
@@ -1,33 +1,36 @@
 package Level;
 
 
+import Control.Menu;
 import Entity.Block.Grass;
 import Entity.Block.Portal;
 import Entity.Entity;
+import Entity.Figure.Bomber;
+import Entity.Figure.Figure;
 import GameRunner.RunBomberman;
 import Graphics.Map;
 import Graphics.Sprite;
 
+import static Control.Menu.*;
 import static GameRunner.RunBomberman.*;
 
 
 public class NextLevel {
-    public static final int numberOfLevel = 3;
+    public static final int numberOfLevel = 2;
 
     public static void nextLevel() {
-
         if (level < numberOfLevel) {
             if (enemy.isEmpty()) {
                 block.add(new Portal(WIDTH - 2, HEIGHT - 2, Sprite.portal.getFxImage()));
-                Entity entity = null;
+//                Entity entity = null;
                 objectMap[HEIGHT - 2][WIDTH - 2] = Map.PORTAL;
                 RunBomberman.wait = true;
-                level++;
+//                level++;
             }
         } else {
-            System.out.println("You win");
-            //Có thể out luôn hoặc là chuyển về menu
-            System.exit(0);
+            if (enemy.isEmpty()) {
+                winGame = true;
+            }
         }
 
     }
